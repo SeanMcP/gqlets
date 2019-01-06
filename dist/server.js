3e74c4eba8e2102c5a01e16c09cab0894d6d3b6b
Sean McPherson <seanmcp25@gmail.com>
2019-01-05 22:53:36 -0500

    Getting errors with @types/graphql
    error TS2583, TS2304

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+// import graphqlHTTP from 'express-graphql';
+// import basicSchema from './schemas/basic';
 var server = express_1.default();
+// var root = {
+//     hello: () => {
+//         return 'Hello world!';
+//     }
+// };
+// server.use(
+//     '/graphql',
+//     graphqlHTTP({
+//         schema: basicSchema,
+//         rootValue: root,
+//         graphiql: true
+//     })
+// );
 server.listen(3000, function () {
     console.log('gqlets listening on port 3000!');
 });
--- server.ts
+++ server.ts
@@ -1,7 +1,25 @@
-import express from 'express'
+import express from 'express';
+// import graphqlHTTP from 'express-graphql';
 
-const server = express()
+// import basicSchema from './schemas/basic';
+
+const server = express();
+
+// var root = {
+//     hello: () => {
+//         return 'Hello world!';
+//     }
+// };
+
+// server.use(
+//     '/graphql',
+//     graphqlHTTP({
+//         schema: basicSchema,
+//         rootValue: root,
+//         graphiql: true
+//     })
+// );
 
 server.listen(3000, () => {
-    console.log('gqlets listening on port 3000!')
+    console.log('gqlets listening on port 3000!');
 });
